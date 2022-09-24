3e8ce0b07000700ef0b78ffa301f755faead213e
Joax <36775053+Joexder@users.noreply.github.com>
2022-09-24 16:12:49 -0400

    Aprendiendo a Usar el body en las request [24/09/2022 16:12]

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 var app = express();
 const router = express.Router();
 
+app.use(bodyParser.json());
 app.use(router);
 
 router.get('/message', function(req, res){
     res.send('Message List');
 });
 
-router.post('/message', (req, res) => res.send('Message Add'));
+router.post('/message', function(req, res){
+    console.log(req.query);
+    res.send('Message Add');
+});
 
 app.listen(3000);
 console.log('La aplicacion esta disponible en: http://localhost:3000/');
