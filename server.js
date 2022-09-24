e8e67e042523844ab2768109414a5946b7261bf6
Joax <36775053+Joexder@users.noreply.github.com>
2022-09-24 13:22:30 -0400

    Aprendiendo sobre el manejador de rutas[24/09/2022 13:22]

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
 
 var app = express();
+const router = express.Router();
 
-app.use('/', function(req, res){
-    res.send('Hola Node');
+app.use(router);
+
+router.get('/message', function(req, res){
+    res.send('Message List');
 });
 
+router.post('/message', (req, res) => res.send('Message Add'));
+
 app.listen(3000);
-console.log('La aplicacion esta disponible en: https://localhost:3000');
+console.log('La aplicacion esta disponible en: http://localhost:3000/');
